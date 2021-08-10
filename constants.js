1ddc9212880ce3ae5c7658fa10306d24669c757d
arc25275 <arc25275@gmail.com>
2021-08-10 22:06:44 +0200

    Initial commit

--- constants.js
+++ constants.js
@@ -6,4 +6,3 @@ module.exports = {
     PC_REALM_API = 'https://pc.realms.minecraft.net',
     BE_REALM_API = 'https://bedrock.realms.minecraft.net'
 };
-
---
+++ status.js
@@ -0,0 +1,2 @@
+const fetch = require("node-fetch");
+const { STATUS_API } = require("./constants");
